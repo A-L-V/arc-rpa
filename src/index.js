17abc5d22bab1102c0f9ba32ab6979ac289531be
Yerco <yerco_123@hotmail.com>
2022-06-22 20:53:11 -0500

    avanze-1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-
+import React, {Component, component} from 'react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.Component>
-    <h1> dfds</h1>
-  </React.Component>
-);
+
+import Noticias from './componentes/Noticias';
+
+class App extends Component{
+
+  state = {
+    noticias :[]
+  }
+
+  componentDidMount(){
+    this.consultaNoticia();
+  }
+
+  consultaNoticia = () => {
+    let url = 'https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=fc13949e4b9541839f4693aa981caed5'
+
+    fetch(url)
+    .then(respuesta =>{
+      return respuesta.json();
+    })
+    .then(noticias =>{
+      console.log(noticias)
+      this.setState({
+        noticias: noticias.articles
+      })
+    })
+  }
+
+  root.render(){
+    return(
+      <div>
+        <div className="container">
+        <Noticias 
+        noticias={this.state.noticias}
+        />
+        </div>
+
+
+      </div>
+    )
+  }
+}
+
+export default App;
 
