65a9726a731e1d202a7b2748f78a4b46c8848149
ALV <=>
2022-07-01 22:58:27 -0500

    Se añadio los 2 planes animado, con su data

--- src/Financiero/Plan.js
+++ src/Financiero/Plan.js
@@ -1,25 +1,39 @@
 import React from "react";
 import './Switch.css'
+import { AiFillCheckCircle } from 'react-icons/ai';
+import data from './PlanesData'
+
 
 
 const Plan = (props) => {
-    const [activado, setActivado] = React.useState('false')
+    const [activado, setActivado] = React.useState('true')
+    const [info, setInfo] = React.useState(data[0])
+
+    const cambiarPlan = () => {
+        if(activado){
+            setInfo(data[1])
+            setActivado(false)
+        }else {
+            setInfo(data[0])
+            setActivado(true)
+        }
+    }
 
     return (
         <div className="align-items-center">
             <nav class="nav">
 
                 <ul className="header-ul position-relative">
-                    <li>
+                    <li className={activado? "fw-bold" : ""} >
                         Plan Estandar
                     </li>
                     <li>
-                        <label className="switch">
-                            <input type="checkbox" className="checkbox" id="mode" />
+                        <label className="switch" >
+                            <input type="checkbox" className="checkbox" id="mode" onClick={cambiarPlan} />
                             <span className="slider"></span>
                         </label>
                     </li>
-                    <li>
+                    <li className={!activado? "fw-bold" : ""} >
                         Plan Premium
                     </li>
                 </ul>
@@ -27,15 +41,15 @@ const Plan = (props) => {
             <div className="container-fluid w-50  ">
                 <div class="row align-items-center">
                     <div class="col ">
-                        <p className="fs-1 strong text-end">S/ 59</p>
+                        <p className="fs-1 fw-bold text-end">{info.precio}</p>
                     </div>
                     <div class="col ">
-                        <p className="fs-3 strong text-start">/Al mes</p>
+                        <p className="fs-3 fw-bold text-start">/Al mes</p>
                     </div>
                 </div>
                 <div class="row align-items-center">
                     <div class="col ">
-                        <p className="text-break text-center">mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+                        <p className="text-break text-center">{info.descripcion}
                         </p>
                     </div>
 
@@ -44,7 +58,20 @@ const Plan = (props) => {
                     <div class="col ">
                         <hr></hr>
                     </div>
+                </div>
 
+                {info.ventajas.map( vent=> {
+                    return <div class="row align-items-center text-center">
+                        <div class="col ">
+                            <AiFillCheckCircle/> {vent}
+                        </div>
+                    </div>
+                })}
+
+                <div class="row align-items-center">
+                    <div class="col ">
+                        <hr></hr>
+                    </div>
                 </div>
 
                 <div class="row align-items-center">
---
+++ src/Financiero/PlanesData.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        "nombre": "Plan Estandar",
+        "descripcion": "Con este plan lo tendras ilimitado con casi todas las opciones permitidas",
+        "precio": "S/ 20",
+        "ventajas": [
+            "Publicidad definida", "3 proyectos", "4 miembros de Equipo"
+        ]
+    },
+    {
+        "nombre": "Plan Premium",
+        "descripcion": "Sin limites a un costo bajo",
+        "precio": "S/ 60",
+        "ventajas" : [
+            "Todo el Plan Estandar",
+            "Libre de Anuncios",
+            "Ilimitado Proyectos"
+        ]
+    }
+];
+export default data;
